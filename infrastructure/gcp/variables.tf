d99188bc0c533f8b6c56b7fad6252a0fde708cbe
Sai Swaroop Rayaprolu <68277702+Swaroop1769@users.noreply.github.com>
2025-03-29 17:02:41 +0530

    Update variables.tf

--- infrastructure/gcp/variables.tf
+++ infrastructure/gcp/variables.tf
@@ -1,24 +1,3 @@
-# variable "gcp_project" {
-#   description = "GCP project ID"
-#   type        = string
-# }
-
-# variable "gcp_region" {
-#   description = "GCP region to deploy resources"
-#   type        = string
-#   default     = "us-central1"
-# }
-
-# variable "gcs_bucket_name" {
-#   description = "Name for the GCS bucket"
-#   type        = string
-# }
-
-# variable "environment" {
-#   description = "Deployment environment"
-#   type        = string
-#   default     = "dev"
-# }
 variable "gcp_project" {
   description = "GCP project ID"
   type        = string
